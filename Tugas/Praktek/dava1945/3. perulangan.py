db492a78214c714e92c116dae21f92ddc5b41635
dav1945 <davagerard87@gmail.com>
2026-04-22 14:48:31 +0800

    Create 3. perulangan.py

---
+++ Tugas/Praktek/dava1945/3. perulangan.py
@@ -0,0 +1,24 @@
+for i in range(7):
+    print("Perulangan ke-", i)
+
+
+i = 2
+while i <= 6:
+    print("Angka:", i)
+    i += 2   
+
+
+for i in range(11):
+    if i == 4:
+        break
+    print(i)
+
+
+for i in range(7):
+    if i == 4:
+        continue
+    print(i)
+
+
+for i in range(6):
+    pass
