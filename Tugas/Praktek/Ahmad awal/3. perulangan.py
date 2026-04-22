979e6c322c087125b3994f39e13d8d0f94716f39
Awl1945 <ahmadawalhawk@gmail.com>
2026-04-22 13:51:01 +0700

    Create 3. perulangan.py

---
+++ Tugas/Praktek/Ahmad awal/3. perulangan.py
@@ -0,0 +1,28 @@
+for i in range(10):
+    print("perulangan ke ", i)
+    
+    
+    
+i = 5
+while i <=8:
+    print("Angka:", i)
+    i += 1
+    
+    
+    
+for i in range (10):
+    if i == 5:
+        break
+    print(i)
+    
+    
+    
+for i in range (10):
+    if i == 1:
+        continue
+    print(i)
+    
+    
+
+for i in range (8):
+    pass
