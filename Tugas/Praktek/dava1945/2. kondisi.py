272a0b4207d96bb74d9140c4d4ee9dc8c8d449d4
dav1945 <davagerard87@gmail.com>
2026-04-22 14:36:01 +0800

    Update 2. kondisi.py

--- Tugas/Praktek/dava1945/2. kondisi.py
+++ Tugas/Praktek/dava1945/2. kondisi.py
@@ -30,39 +30,3 @@ nilai = 80
 absen = 90
 if nilai >= 75 and absen >= 80:
     print("Lulus")
-    
-    
- 
- 
-    
-for i in range(5):
-    print("Perulangan ke-", i)
-    
-    
-i = 1
-while i <= 5:
-    print("Angka:", i)
-    i += 1
-    
-   
-   
-    
-for i in range(10):
-    if i == 5:
-        break
-    print(i)
-
-    
-    
-    
-    
-for i in range(5):
-    if i == 2:
-        continue
-    print(i)
-    
-    
-    
-    
-for i in range(5):
-    pass
