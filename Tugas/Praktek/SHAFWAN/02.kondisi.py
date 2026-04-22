ea145d6acc6e7147339da93efdbb1491be4f70b7
shafwan <andishafwanshafwan36@gmail.com>
2026-04-22 14:38:26 +0800

    Create 02.kondisi.py

---
+++ Tugas/Praktek/SHAFWAN/02.kondisi.py
@@ -0,0 +1,29 @@
+nilai=90
+if nilai>=80:
+    print("lulus")
+    
+    
+    
+nilai=90
+if nilai>=80:
+    print("lulus")
+else:
+    print("tidak lulus")
+    
+    
+    
+nilai=90
+if nilai>=80:
+    print("A")
+elif nilai>=70:
+    print("B")
+elif nilai>=60:
+    print("C")
+else:
+    print("D")
+    
+    
+nilai=90
+absen=80
+if nilai>=90 and absen>=80:
+    print("lulus")
