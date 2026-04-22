757c93a1eded0fa6438abfae9ec6562af41a864e
Awl1945 <ahmadawalhawk@gmail.com>
2026-04-22 13:23:56 +0700

    Create 2. kondisi.py

---
+++ Tugas/Praktek/Ahmad awal/2. kondisi.py
@@ -0,0 +1,25 @@
+nilai = int(input("masukkan nilai :"))
+if nilai >= 75:
+   print("lulus")
+   
+   
+   
+nilai_kamu = int(input("masukkan nilai kamu :"))
+if nilai_kamu >= 75:
+   print("lulus")
+else:
+    print("tidak lulus")
+   
+
+
+Nilaimu = int(input("masukkan nilaimu :"))
+if Nilaimu >= 75:
+   print("A")
+elif Nilaimu >= 80:
+   print("B")
+elif Nilaimu >= 70:
+   print("C")
+elif Nilaimu >= 60:
+   print("D")
+else:
+    print("E")
