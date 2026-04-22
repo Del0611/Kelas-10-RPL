1aa1792482ed9a37b44c9a33fe046f119f7a2b65
Pompom <166188511+PapamTheBestFR@users.noreply.github.com>
2026-04-21 23:28:21 -0700

    Create 02.Kondisi percabangan.py

---
+++ Tugas/Praktek/farhan/02.Kondisi percabangan.py
@@ -0,0 +1,32 @@
+ #1. if
+nilai = 80
+
+if nilai >= 75:
+    print("Lulus")
+
+ #2. if - else
+nilai = 60
+
+if nilai >= 75:
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+    
+ #3 - elif - else
+nilai = 85
+
+if nilai >= 90:
+    print("A")
+elif nilai >= 80:
+    print("B")
+elif nilai >= 70:
+    print("C")
+else:
+    print("D")
+    
+ #4. Operator Logika
+nilai = 80
+absen = 90
+
+if nilai >= 75 and absen >= 80:
+    print("Lulus")
