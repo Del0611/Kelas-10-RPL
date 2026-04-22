f79fb908c368421cc27548703b3d1be3b470c0f1
shafwan <andishafwanshafwan36@gmail.com>
2026-04-22 15:18:53 +0800

    Update and rename 01.perulangan PY to 01.perulangan.py

---
+++ Tugas/Praktek/SHAFWAN/01.perulangan.py
@@ -0,0 +1,16 @@
+for i in range (10):
+    if i == 5:
+        break
+    print(i)
+    
+    
+    
+for i in range (10):
+    if i == 1:
+        continue
+    print(i)
+    
+    
+
+for i in range (8):
+    pass
